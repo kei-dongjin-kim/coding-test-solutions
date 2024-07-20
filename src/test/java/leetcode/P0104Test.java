9d2a77552046c444800f53adc3a39282329a6e5f
kei.dongjin.kim <kei.dongjin.kim@gmail.com>
2024-07-20 21:08:41 +0200

    Solved

---
+++ src/test/java/leetcode/P0104Test.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class P0104Test {
+
+    @Test
+    @DisplayName("root = [1,2,3,4,5,6,7]")
+    void test1() {
+        TreeNode root =
+            new TreeNode(1,
+                new TreeNode(2,
+                    new TreeNode(4),
+                    new TreeNode(5)
+                ),
+                new TreeNode(3,
+                    new TreeNode(6),
+                    new TreeNode(7)
+                )
+            );
+        assert new P0104().maxDepth(root) == 3;
+    }
+}
