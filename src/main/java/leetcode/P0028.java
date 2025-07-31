0201d72386e2a45d9fc06454cc5b2cc6b29fb694
kei.dongjin.kim <kei.dongjin.kim@gmail.com>
2025-07-31 19:57:19 +0900

    Solved

--- src/main/java/leetcode/P0028.java
+++ src/main/java/leetcode/P0028.java
@@ -2,6 +2,22 @@ package leetcode;
 
 public class P0028 {
     public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
+        int len_h = haystack.length();
+        int len_n = needle.length();
+        for (int i = 0; i < len_h - len_n + 1; i++) {
+            if (haystack.charAt(i) == needle.charAt(0)) {
+                boolean flag = true;
+                for (int j = 0; j < len_n; j++) {
+                    if (haystack.charAt(i + j) != needle.charAt(j)) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    return i;
+                }
+            }
+        }
+        return -1;
     }
 }
