50c5ecceab5d0ff71843a63a05ba9ab7524b8e59
kei.dongjin.kim <kei.dongjin.kim@gmail.com>
2025-08-27 19:04:11 +0900

    Solved

--- src/main/java/leetcode/P3226.java
+++ src/main/java/leetcode/P3226.java
@@ -1,4 +1,26 @@
 package leetcode;
 
 public class P3226 {
+    public int minChanges(int n, int k) {
+        if (n == k) {
+            return 0;
+        }
+        int count = 0;
+        while (n != 0 || k != 0) {
+            int nr = n % 2;
+            int kr = k % 2;
+            if (nr == kr) {
+                // Do nothing.
+            } else {
+                if (nr == 1 && kr == 0) {
+                    count++;
+                } else if (nr == 0 && kr == 1) {
+                    return -1;
+                }
+            }
+            n /= 2;
+            k /= 2;
+        }
+        return count;
+    }
 }
--- src/test/java/leetcode/P3226Test.java
+++ src/test/java/leetcode/P3226Test.java
@@ -1,4 +1,17 @@
 package leetcode;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class P3226Test {
+
+    @Test
+    void test1() {
+        int n = 31;
+        int k = 21;
+        int expected = 2;
+        P3226 solution = new P3226();
+        int actual = solution.minChanges(n, k);
+        Assertions.assertEquals(expected, actual);
+    }
 }
